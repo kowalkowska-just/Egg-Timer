afa6995a62542e63cf8762a3d4da945f877b13b3
kowalkowska-just <kowalkowska.just@gmail.com>
2020-06-30 19:10:59 +0200

    Complete app with auto layout

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,14 +7,58 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var player: AVAudioPlayer!
+    var eggTimes = ["Soft" : 3, "Medium" : 4, "Hard" : 7]
+    
+    var secondsRemaining = 60
+    var timeEgg: Int = 0
+    var timer = Timer()
+    
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var proressView: UIProgressView!
+    
+    
+    @IBAction func hardnessSelection(_ sender: UIButton) {
+        
+        proressView.progress = 0.0
+        let hardness = sender.currentTitle! //Soft, Medium, Hard
+        
+        textLabel.text = hardness
+        
+        secondsRemaining = eggTimes[hardness]!
+        timeEgg = eggTimes[hardness]!
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
-
+    
+    func PlaySound(soundName: String){
+        let mp3 = Bundle.main.url(forResource: soundName, withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: mp3!)
+        player.play()
+    }
+    
+    @objc func updateTimer() {
+        if secondsRemaining > 0 {
+            var procent: Float
+            var progress: Float
+            
+            procent = Float(secondsRemaining) / Float(timeEgg)
+            progress = 1 - procent
+            proressView.progress = progress
+            
+            print("\(secondsRemaining) seconds.")
+            secondsRemaining -= 1
+        } else {
+            timer.invalidate()
+            textLabel.text = "Done!"
+            proressView.progress = 1.0
+            PlaySound(soundName: "alarm_sound")
+            }
+        }
+    
 
 }
 
